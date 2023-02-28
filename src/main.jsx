9e94bea514732c09d5c9da267a3050ad04b45ad9
krisikostadinov <krisi.199898@gmail.com>
2023-02-28 12:40:20 +0200

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,68 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import { Home, About, Gallery, Contacts, Rouds } from "./pages/index";
-import MainLayout from "./layouts/MainLayout";
-import Street from "./pages/asphaltings/Street";
-import Yard from "./pages/asphaltings/Yard";
-import Alley from "./pages/asphaltings/Alley";
-import Sidewalk from "./pages/asphaltings/Sidewalk";
-import SmallAndBig from "./pages/asphaltings/SmallAndBig";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/za-nas",
-        element: <About />,
-      },
-      {
-        path: "/contacts",
-        element: <Contacts />,
-      },
-      {
-        path: "/gallery",
-        element: <Gallery />,
-      },
-      {
-        path: "/asfaltirane-na-patishta",
-        element: <Rouds />,
-      },
-      {
-        path: "/asfaltirane-na-ulitsi",
-        element: <Street />,
-      },
-      {
-        path: "/afaltirane-na-dvor",
-        element: <Yard />,
-      },
-      {
-        path: "/asfaltirane-na-alei",
-        element: <Alley />,
-      },
-      {
-        path: "/narezhdane-na-trotoar",
-        element: <Sidewalk />,
-      },
-      {
-        path: "/asfaltirane-na-malki-i-golemi-ploshti",
-        element: <SmallAndBig />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
-  </>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home";
-export { default as About } from "./About";
-export { default as Gallery } from "./Gallery";
-export { default as Contacts } from "./Contacts";
-export { default as Rouds } from "./asphaltings/Rouds";
